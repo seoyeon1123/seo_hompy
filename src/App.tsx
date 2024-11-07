dbecad8eb4c82c0a7353c37430877b29efcbdf2b
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2024-11-07 13:40:50 +0900

    Fix: vercel 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Layout from '../src/layout/Layout';
+import Layout from './layout/Layout';
 
 export default function App() {
   return (
