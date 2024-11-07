17c823309df8a02b1242feff929099e8b9081bb0
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2024-11-07 13:25:03 +0900

    Feat: 초기화

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,24 +1,12 @@
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
 import Navigation from './Navigation';
 
 const Layout = () => {
   return (
-    <div className="flex flex-col md:flex-row h-screen justify-center items-center gap-0.5">
-      {/* Sidebar section */}
-      <div className="w-full md:w-auto md:h-4/5 bg-yellow-300 p-10 md:pr-0 rounded-lg shadow-md">
-        <Sidebar />
-      </div>
-
-      {/* Outlet and Navigation section */}
-      <div className="relative w-full md:w-3/5 md:h-4/5 bg-yellow-300 p-10 md:pl-0 rounded-lg shadow-md flex flex-col md:flex-row">
-        <Outlet />
-
-        <div className="absolute md:top-20 md:-right-[73px] right-0 z-10">
-          <Navigation />
-        </div>
-      </div>
-    </div>
+    <>
+      <Navigation />
+      <Outlet />
+    </>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,86 +1,78 @@
 import { useState } from 'react';
-import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
-
-const techBadges = [
-  'https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
-  'https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white',
-  'https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black',
-  'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
-  'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white',
-  'https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=Next.js&logoColor=white',
-  'https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white',
-  'https://img.shields.io/badge/Tailwind CSS-06B6D4?style=for-the-badge&logo=Tailwind CSS&logoColor=white',
-  'https://img.shields.io/badge/Firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=black',
-  'https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white',
-  'https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=GitHub&logoColor=white',
-  'https://img.shields.io/badge/Jest-%23C21325?style=for-the-badge&logo=jest&logoColor=white',
-];
+import { motion } from 'framer-motion';
+import profile from '../assets/main/IMG_6067.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
-  const [badges, setBadges] = useState(techBadges);
-
-  const handleDragEnd = ({ destination, source }: DropResult) => {
-    // destination이 없으면 종료
-    if (!destination) return;
-
-    setBadges((oldBadges) => {
-      const badges = Array.from(oldBadges);
-      const [removed] = badges.splice(source.index, 1);
-      badges.splice(destination.index, 0, removed);
-
-      return badges;
-    });
-  };
+  const [firstTextVisible, setFirstTextVisible] = useState(false);
+  const [secondTextVisible, setSecondTextVisible] = useState(false);
+  const [thirdTextVisible, setThirdTextVisible] = useState(false);
 
   return (
-    <>
-      <div className="bg-neutral-100 h-full rounded-lg p-5">
-        <div>
-          <h1 className="text-2xl font-semibold mb-2">서연이의 미니홈피</h1>
-          <div className="flex flex-row gap-3 bg-white p-3 rounded-lg justify-around">
-            <div className="w-2/5">
-              <h2 className="text-blue-500 text-2xl">Introduction</h2>
-              <div className="border-t border-gray-300 my-2 border-dashed" />
-              <div className="flex flex-col gap-2">
-                <p className="text-sm">
-                  안녕하세요. 보육교사에서 개발자로 전향 중인 <br />
-                  멋쟁이 이서연 입니다.
-                </p>
-                <p>・ 취미 : 운동</p>
-                <p>・ 특기 : 만들기</p>
-              </div>
-            </div>
-            <div className="w-3/5">
-              <h2 className="text-blue-500 text-2xl">기술 스택</h2>
-              <div className="border-t border-gray-300 my-2 border-dashed" />
-              <DragDropContext onDragEnd={handleDragEnd}>
-                <Droppable droppableId="badges" direction="horizontal">
-                  {(provided) => (
-                    <div className="flex flex-wrap gap-3" ref={provided.innerRef} {...provided.droppableProps}>
-                      {badges.map((badge, index) => (
-                        <Draggable key={`draggable-${index}`} draggableId={`badge-${index}`} index={index}>
-                          {(provided) => (
-                            <div
-                              ref={provided.innerRef}
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                              className="transition-transform duration-200 ease-in-out"
-                            >
-                              <img src={badge} alt={`badge-${index}`} className="cursor-pointer" />
-                            </div>
-                          )}
-                        </Draggable>
-                      ))}
-                      {provided.placeholder}
-                    </div>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center gap-5 h-screen bg-[#16423C] text-white relative">
+      {firstTextVisible && (
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
+          className="text-center absolute top-[140px]" // 이미지로부터 80px 위에 텍스트 배치
+          onAnimationComplete={() => setSecondTextVisible(true)}
+        >
+          <h1 className="text-9xl font-bold text-[#6A9C89] relative z-0 text-shadow-lg">Front - End</h1>
+          <h2 className="text-4xl font-medium mb-4 text-neutral-200 absolute top-[90px] left-1/2 w-full transform -translate-x-1/2 z-10">
+            "예비" 프론트엔드 개발자 <span className="text-4xl font-bold">이서연</span> 입니다.
+          </h2>
+        </motion.div>
+      )}
+
+      <motion.img
+        src={profile}
+        alt="프로필"
+        width={300}
+        className="rounded-full shadow-xl border-4 border-[#387478] mb-6"
+        initial={{ x: -800, opacity: 0, rotate: -360 }}
+        animate={{ x: 0, opacity: 1, rotate: 0 }}
+        transition={{ type: 'spring', duration: 6, bounce: 0.5 }}
+        style={{ position: 'absolute' }} // 이미지 고정
+        onAnimationComplete={() => setFirstTextVisible(true)}
+      />
+
+      {secondTextVisible && (
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="text-center absolute bottom-[250px]"
+          onAnimationComplete={() => setThirdTextVisible(true)}
+        >
+          <p className="text-lg text-center px-4 text-[#E9EFEC]">
+            호기심이 많고 새로운 도전을 좋아하는 <span className="text-[#6A9C89] font-semibold">프론트엔드 개발자</span>
+            입니다.
+            <br /> 좋은 사용자 경험을 제공하기 위해 열심히 노력하고 있어요!
+          </p>
+        </motion.div>
+      )}
+
+      {thirdTextVisible && (
+        <motion.div
+          className="bottom-20 absolute mb-3"
+          initial={{ opacity: 0, y: 0 }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            color: ['#6A9C89', '#E9EFEC'],
+          }}
+          transition={{
+            duration: 2, // 색상이 변화하는 속도
+            repeat: Infinity,
+            repeatType: 'reverse',
+          }}
+        >
+          <FontAwesomeIcon icon={faAnglesDown} className="text-4xl" />
+        </motion.div>
+      )}
+    </div>
   );
 };
 
