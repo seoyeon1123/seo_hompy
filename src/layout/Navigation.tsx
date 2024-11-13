7c31760b314de1c0fb23f3554efcca4b79892c4e
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2024-11-13 22:11:23 +0900

    Feat: build error 수정

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface NavigationProps {
   scrollToSection: (sectionId: 'project' | 'education' | 'connect') => void;
   activeSection: 'project' | 'education' | 'connect';
