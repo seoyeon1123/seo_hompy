25553ed3a64e50529a52d0d8ed86ea862257126f
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2024-11-20 13:59:06 +0900

    Feat: build error 수정

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import ProjectCard from '../components/ProjectCard';
 import supabase from '../supabaseClient';
